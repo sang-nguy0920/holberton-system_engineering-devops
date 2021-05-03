fce40ea59acebfb98cd4d2e5fb6b6a40992ef4ce
sang-nguy0920 <2321@holbertonschool.com>
2021-05-03 18:44:08 +0000

    fixed task 0 for pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,12 +15,12 @@ if __name__ == "__main__":
     employee = requests.get("https://jsonplaceholder.typicode.com/users/{}".
                             format(employee_ID), verify=False).json()
 
-    todo_list = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(employee_ID), verify=False).json()
+    td = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                      format(employee_ID), verify=False).json()
     completed = []
-    for task in todo_list:
+    for task in td:
         if task.get('completed') is True:
             completed.append(task.get('title'))
     print("Employee {} is done with tasks({}/{}):".
-          format(employee.get('name'), len(completed), len(todo_list)))
+          format(employee.get('name'), len(completed), len(td)))
     print("\n".join("\t {}".format(task) for task in completed))
