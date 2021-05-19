316f8e45c22d2a74c64a19fda8f47f75e18e075a
sang-nguy0920 <2321@holbertonschool.com>
2021-05-19 20:58:07 +0000

    pep8 fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-A function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit.
+A function that queries the Reddit API and returns
+the number of subscribers (not active users, total subscribers)
+for a given subreddit.
 """
 
 import json
